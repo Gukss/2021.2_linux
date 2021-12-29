f303f58daa9b03def15a386c000032909605103f
Gukss <dongkuk97@naver.com>
2021-12-29 20:46:24 +0900

    복잡한 ls 구현

---
+++ 12주/myls234.c
@@ -0,0 +1,309 @@
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+
+typedef struct Node {
+    struct Node *next;
+    char dirpath[256];
+}Node;
+
+void append(Node *list, char *newpath)
+{
+    if(list->next == NULL)
+    {
+        Node *newNode = malloc(sizeof(Node));
+        strcpy(newNode->dirpath, newpath);
+        newNode->next = NULL;
+        list->next = newNode;
+    }
+    else
+    {
+        Node *cur = list;
+        while(cur->next != NULL)
+        {
+            cur = cur->next;
+        }
+        Node *newNode = malloc(sizeof(Node));
+        strcpy(newNode->dirpath, newpath);
+        newNode->next = NULL;
+        cur->next = newNode;
+    }
+}
+
+int basic(unsigned int flag, char *path)
+{
+    DIR *dir_ptr = NULL;
+    struct dirent *dentry = NULL;
+    struct stat statbuf;
+    char fullpath[256];
+    struct tm *time;
+    // int dirR = 0;
+    // int i=0;
+    Node *list1 = malloc(sizeof(Node));
+    list1->next = NULL;
+
+    if ((dir_ptr = opendir(path)) == NULL)
+    {
+        fprintf(stderr, "dir open error\n");
+    }
+
+    //옵션 R 처리
+    if ((flag & 0x20) != 0)
+    {
+        printf("%s:\n", path);
+    }
+
+    while (dentry = readdir(dir_ptr))
+    {
+        if (dentry->d_ino == 0)
+        {
+            continue;
+        }
+        //옵션 a 처리
+        if ((flag & 0x01) == 0)
+        {
+            if (strncmp(dentry->d_name, ".", 1) == 0)
+            {
+                continue;
+            }
+        }
+        strcpy(fullpath, path);
+        strcat(fullpath, "/");
+        strcat(fullpath, dentry->d_name);
+
+        if (lstat(fullpath, &statbuf) < 0)
+        {
+            fprintf(stderr, "stat error\n");
+        }
+
+        //옵션 i 처리
+        if ((flag & 0x02) != 0)
+        {
+            printf("%7ld ", dentry->d_ino);
+        }
+
+        //옵션 s 처리
+        if ((flag & 0x04) != 0)
+        {
+            printf("%7ld ", statbuf.st_size / 1024);
+        }
+
+        //옵션 l 처리
+        if ((flag & 0x10) != 0)
+        {
+            //파일 유형
+            switch (statbuf.st_mode & S_IFMT)
+            {
+            case S_IFREG:
+                printf("-");
+                break;
+            case S_IFDIR:
+                printf("d");
+                break;
+            case S_IFIFO:
+                printf("p");
+                break;
+            case S_IFLNK:
+                printf("l");
+                break;
+            case S_IFCHR:
+                printf("c");
+                break;
+            case S_IFBLK:
+                printf("b");
+                break;
+            case S_IFSOCK:
+                printf("s");
+                break;
+            default:
+                break;
+            }
+
+            //소유자권한
+            if ((S_IRUSR & statbuf.st_mode) != 0)
+                printf("r");
+            else
+                printf("-");
+            if ((S_IWUSR & statbuf.st_mode) != 0)
+                printf("w");
+            else
+                printf("-");
+            if ((S_IXUSR & statbuf.st_mode) != 0)
+                printf("x");
+            else
+                printf("-");
+            //그룹 권한
+            if ((S_IRGRP & statbuf.st_mode) != 0)
+                printf("r");
+            else
+                printf("-");
+            if ((S_IWGRP & statbuf.st_mode) != 0)
+                printf("w");
+            else
+                printf("-");
+            if ((S_IXGRP & statbuf.st_mode) != 0)
+                printf("x");
+            else
+                printf("-");
+            //기타 권한
+            if ((S_IROTH & statbuf.st_mode) != 0)
+                printf("r");
+            else
+                printf("-");
+            if ((S_IWOTH & statbuf.st_mode) != 0)
+                printf("w");
+            else
+                printf("-");
+            if ((S_IXOTH & statbuf.st_mode) != 0)
+                printf("x");
+            else
+                printf("-");
+            printf(" ");
+
+            //link
+            printf("%2ld ", statbuf.st_nlink);
+
+            //사용자명
+            printf("%s ", getpwuid(statbuf.st_uid)->pw_name);
+            //그룹명
+            printf("%s ", getgrgid(statbuf.st_gid)->gr_name);
+            //파일 사이즈
+            printf("%7ld ", statbuf.st_size);
+            //파일 접근시간
+            time = localtime(&statbuf.st_mtime);
+            printf("%2d월 %2d %2d:%2d ", time->tm_mon + 1, time->tm_mday, time->tm_hour, time->tm_min);
+        }
+        //기본 출력
+        printf("%s", dentry->d_name);
+
+        //옵션 F 처리
+        if ((flag & 0x08) != 0)
+        {
+            if (S_ISDIR(statbuf.st_mode))
+                printf("/");
+            else if (S_ISSOCK(statbuf.st_mode))
+                printf("=");
+            else if (S_ISFIFO(statbuf.st_mode))
+                printf("|");
+            else if (S_ISLNK(statbuf.st_mode))
+                printf("@");
+            else if ((S_IXUSR & statbuf.st_mode) != 0)
+                printf("*");
+        }
+        printf("\n");
+
+        //옵션 R 처리
+        if ((flag & 0x20) != 0)
+        {
+            if (S_ISDIR(statbuf.st_mode))
+            {
+                //링크드리스트로 구현하기
+                append(list1, fullpath);
+            }
+        }
+    }
+    printf("\n");
+    //옵션 R 처리
+    if ((flag & 0x20) != 0)
+    {
+        Node *cur = list1->next;
+        while(cur != NULL)
+        {
+            basic(flag, cur->dirpath);
+            cur = cur->next;
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int opt = 0;
+    unsigned int flag = 0;
+    char dir[256][256];
+    char path[256];
+    int i = 0;
+
+    while ((opt = getopt(argc, argv, "aisFlR")) != -1)
+    {
+        switch (opt)
+        {
+        case 'a':
+            if ((flag & 0x01) == 0)
+            {
+                flag |= 0x01;
+            }
+            break;
+        case 'i':
+            if ((flag & 0x02) == 0)
+            {
+                flag |= 0x02;
+            }
+            break;
+        case 's':
+            if ((flag & 0x04) == 0)
+            {
+                flag |= 0x04;
+            }
+            break;
+        case 'F':
+            if ((flag & 0x08) == 0)
+            {
+                flag |= 0x08;
+            }
+            break;
+        case 'l':
+            if ((flag & 0x10) == 0)
+            {
+                flag |= 0x10;
+            }
+            break;
+        case 'R':
+            if ((flag & 0x20) == 0)
+            {
+                flag |= 0x20;
+            }
+            break;
+        default:
+            break;
+        }
+    }
+    int j = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        if (argv[i][0] == '-')
+        {
+            continue;
+        }
+        else
+        {
+            strcpy(dir[j], argv[i]);
+            j++;
+        }
+    }
+    //인자가 없는 경우
+    i = 0;
+    if (strlen(dir[i]) == 0)
+    {
+        strcpy(dir[i], ".");
+    }
+    while (strlen(dir[i]))
+    {
+        chdir(dir[i]);
+        getcwd(path, 256);
+        if (argc > 2)
+        {
+            printf("%s:\n", path);
+        }
+        basic(flag, path);
+        i++;
+        printf("\n");
+    }
+}
